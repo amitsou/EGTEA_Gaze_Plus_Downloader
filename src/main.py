08cf7c79a4da3cc15c2b468b0199620fc96ff048
amitsou <amitsou95@gmail.com>
2024-02-26 22:31:04 +0200

    added new downloader files

---
+++ src/main.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+if __name__ == '__main__':
+    pass
+
+#TODO: Get the download links using scrapy
+#TODO: Download the files using requests and save them in the data folder or in a specified directory
+#TODO: Extract the files using the zipfile module
