6a3a78490414b45e67d18590a104031ae7f60e8a
amitsou <amitsou95@gmail.com>
2024-03-01 21:47:39 +0200

    major refactor

--- src/scrapper/scrapper/items.py
+++
@@ -1,7 +0,0 @@
-import scrapy
-
-
-class ScrapperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
--- src/scrapper/scrapper/pipelines.py
+++
@@ -1,7 +0,0 @@
-from itemadapter import ItemAdapter
-import subprocess
-
-
-class ScrapperPipeline:
-    def process_item(self, item, spider):
-        return item
